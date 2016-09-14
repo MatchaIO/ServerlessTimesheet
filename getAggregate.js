a827ad4aec1dc4b8131e829df560d09dce51185a
RhysC <rhysevancampbell@gmail.com>
2016-09-14 22:50:16 +0800

    Clean up the getAggregate smaple

--- getAggregate.js
+++ getAggregate.js
@@ -1,91 +1,58 @@
 "use strict";
-var AWS = require("aws-sdk");
-var validate = require("validate.js");
+let AWS = require("aws-sdk");
 AWS.config.update({
   region: "ap-southeast-2"
 });
-const SEED_VERSION = 0;
-class AggregateBase{
-  constructor(uuid) {
-    this.id = uuid;
-    this.version = SEED_VERSION;
-    this._uncommittedEvents = [];
-    this._routeEvents = {};//Must be overridden in inherited classes
-  };
-  }
-}
-var Timesheet = function(uuid){
-  var self = this;
-  
-  self.id = uuid;
-  self.version = SEED_VERSION;
-  self._uncommittedEvents = [];
-  _routeEvents = {
-    "TimesheetCreated": function(timesheetCreated){} 
-  };
-  
-  self._applyEvent = function(record){
-    var route = _routeEvents[record.eventsMetadata.eventType];
-    route(record.event);
-    ++ self.version;
-  }
-  self._raiseEvent = function(event){
-    var mappedEvent = {
-      "id": event.id,
-      "eventId": event.eventId,
-      "eventsMetadata": {
-        "aggregateType": "Timesheet",
-        "eventType": event.eventType,
-        "timestamp": new Date(),
-        "sourceLambdaEvent": event.sourceLambdaEvent
-      },
-      "event" : event.event
-    };    
-    self._applyEvent(mappedEvent);
-    self._uncommittedEvents.push(mappedEvent);
-  }
-  self.create = function(timesheetCreatedPayload){
-    if (self.version != SEED_VERSION) throw new InvalidOperationException("Create can only be executed as the first action.");
-    var timesheetCreatedEvent = {
-      "id": uuid.v1(),
-      "eventId": 1,
-      "eventType": "TimesheetCreated",
-      "sourceLambdaEvent": JSON.stringify(timesheetCreatedPayload),
-      "event" : timesheetCreatedPayload.body
-    };
-    self._raiseEvent(timesheetCreatedEvent);
-  }
-}
+//https://blogs.aws.amazon.com/javascript/post/Tx3BZ2DC4XARUGG/Support-for-Promises-in-the-SDK
+AWS.config.setPromisesDependency(require('Q').Promise)
+
+let Timesheet = require('./TimesheetAggregate');
 
 let docClient = new AWS.DynamoDB.DocumentClient();
-console.log(docClient);
 console.log("Querying for aggregate.");
-var timesheetId = "c799d740-78e9-11e6-95b0-31eed3ff8b95";
-var params = {
-    TableName : "Timesheets",
-    KeyConditionExpression: "id = :v_id",
-    ExpressionAttributeValues: {
-        ":v_id":timesheetId
-    }
-};
-docClient.query(params, function(err, data) {
-    if (err) {
-        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Query succeeded.");
-        data.Items.forEach(function(item) {
-          console.log("-------------------------");
-          console.log(item);            
-        });
-    }
-});
+// let timesheetId = "c799d740-78e9-11e6-95b0-31eed3ff8b95";
+let timesheetId = "c799d740-78e9-11e6-95b0-";
+let params = getParametersFor("Timesheets", timesheetId)
+let timesheet = new Timesheet(timesheetId);
+// docClient.query(params, function(err, data) {
+    // if (err) {
+        // console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    // } else {        
+        // data.Items.forEach(function(item) {
+          // timesheet._applyEvent(item)          
+        // });
+        // console.log(timesheet)
+    // }
+// });
 
-function InvalidOperationException(value) {
-   this.value = value;
-   this.message = "does not conform to the expected format for a zip code";
-   this.toString = function() {
-      return this.value + this.message;
+docClient.query(params).promise()
+         .then((data)=> {
+            if(!(data && data.Items) || data.Items.length == 0) {
+              throw new AggregateNotFoundException("Timesheets", timesheetId);
+            }
+            data.Items.forEach(function(item) {
+              timesheet._applyEvent(item)          
+            });
+            console.log(timesheet)
+          })
+          .catch((error) => {
+            console.error("Unable to query. Error: " + error, JSON.stringify(error, null, 2));
+          })
+          .done();
+
+function AggregateNotFoundException(tableName, id) {
+   this.tableName = tableName;
+   this.id = id;
+   this.toString = () => {
+      return "Can not find aggregate from table '" + this.tableName + "' with id '" + this.id +"'.";
    };
 }
-
+function getParametersFor(tableName, id){
+  // we could pass in the aggregate here and get the table name (aggregateInstance.constructor.name +'s') and id (aggregateInstance.id)
+  return {
+    TableName : tableName,
+    KeyConditionExpression: "id = :v_id",
+    ExpressionAttributeValues: { ":v_id": id }
+  };
+}
 
