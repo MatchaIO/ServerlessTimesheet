a7b0ffb095750e8f1c664175ab3aa60f128bf950
RhysC <rhysevancampbell@gmail.com>
2016-09-18 15:26:56 +0800

    Update and create tests added

---
+++ test/Timesheets_test.js
@@ -0,0 +1,142 @@
+"use strict";
+var uuid = require('node-uuid');
+var Timesheet = require('../TimesheetAggregate');
+var assert = require('assert');
+describe('Timesheet', function() {
+  describe('create', function() {
+    it('raise a timesheetCreatedEvent', function() {
+      let tsId = uuid.v1();
+      let t = new Timesheet(tsId);
+      let cmd = create_createTimesheetCommand();
+      t.create(cmd);
+      assert(t._uncommittedEvents.length == 1, "expected a sinlge uncommited event");
+      let createdEvent = t._uncommittedEvents[0];
+      assert.deepEqual(cmd.body, createdEvent.event)
+      assert.equal(createdEvent.id,  tsId);
+      assert.equal(createdEvent.eventId,  1);
+      assert.equal(createdEvent.eventsMetadata.eventType,  "TimesheetCreated");
+      assert.equal(createdEvent.eventsMetadata.aggregateType,  "Timesheet");
+    });
+    it('cannot send create command more than once', function() {
+      let tsId = uuid.v1();
+      let t = new Timesheet(tsId);
+      t.create(create_createTimesheetCommand());
+      assert.throws(
+        () => {
+           t.create(create_createTimesheetCommand());
+        },
+        "InvalidOperationException: Create can only be executed as the first action."
+      );
+    });
+  });
+  describe('update', function() {
+    it('raise a timesheetUpdatedEvent', function() {
+      let tsId = uuid.v1();
+      let t = new Timesheet(tsId);
+      t.create(create_createTimesheetCommand());
+      let cmd = create_updateTimesheetCommand();
+      t.update(cmd);
+      assert(t._uncommittedEvents.length == 2, "expected 2 uncommited events");
+      let updatedEvent = t._uncommittedEvents[1];
+      assert.deepEqual(cmd.body, updatedEvent.event)
+      assert.equal(updatedEvent.id,  tsId);
+      assert.equal(updatedEvent.eventId,  2);
+      assert.equal(updatedEvent.eventsMetadata.eventType,  "TimesheetUpdated");
+      assert.equal(updatedEvent.eventsMetadata.aggregateType,  "Timesheet");
+    });
+    it('cannot send update command as first command', function() {
+      let tsId = uuid.v1();
+      let t = new Timesheet(tsId);
+      assert.throws(
+        () => {
+           t.update(create_updateTimesheetCommand());
+        },
+        "InvalidOperationException: Can not update an uninitialised Aggregate, update can not be executed as the first action."
+      );
+    });
+  });
+  
+  function create_createTimesheetCommand(){
+    return { 
+      "body":{
+        "shifts" : [{
+            "name" : "Monday",
+            "startDateTime" : "2016-09-05T09:00:00+08:00",
+            "endDateTime" : "2016-09-05T17:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Tuesday",
+            "startDateTime" : "2016-09-06T09:00:00+08:00",
+            "endDateTime" : "2016-09-06T17:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Wednesday",
+            "startDateTime" : "2016-09-07T09:00:00+08:00",
+            "endDateTime" : "2016-09-07T17:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Thursday",
+            "startDateTime" : "2016-09-08T09:00:00+08:00",
+            "endDateTime" : "2016-09-08T17:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Friday",
+            "startDateTime" : "2016-09-09T09:00:00+08:00",
+            "endDateTime" : "2016-09-09T16:00:00+08:00",
+            "breaks" : [3600000],
+            "notes" : {
+              "text" : "Left early for a beer",
+              "isPrivate" : true
+            }
+          }
+        ],
+        "notes" : {
+          "text" : "Remind the boss to approve",
+          "isPrivate" : true
+        }
+      }
+    }
+  }
+  
+  function create_updateTimesheetCommand(){
+    return { 
+      "body":{
+        "shifts" : [{
+            "name" : "Monday",
+            "startDateTime" : "2016-09-05T10:00:00+08:00",
+            "endDateTime" : "2016-09-05T18:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Tuesday",
+            "startDateTime" : "2016-09-06T10:00:00+08:00",
+            "endDateTime" : "2016-09-06T18:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Wednesday",
+            "startDateTime" : "2016-09-07T10:00:00+08:00",
+            "endDateTime" : "2016-09-07T18:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Thursday",
+            "startDateTime" : "2016-09-08T10:00:00+08:00",
+            "endDateTime" : "2016-09-08T18:00:00+08:00",
+            "breaks" : [3600000]
+          }, {
+            "name" : "Friday",
+            "startDateTime" : "2016-09-09T07:00:00+08:00",
+            "endDateTime" : "2016-09-09T15:00:00+08:00",
+            "breaks" : [3600000],
+            "notes" : {
+              "text" : "Left early for training",
+              "isPrivate" : true
+            }
+          }
+        ],
+        "notes" : {
+          "text" : "Remind payroll to transfer funds to new account",
+          "isPrivate" : true
+        }
+      }
+    }
+  }
+});
