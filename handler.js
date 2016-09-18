741254f73c321203261ceb39eb17ab9e9be39a26
RhysC <rhysevancampbell@gmail.com>
2016-09-18 22:53:09 +0800

    rest sample highlighted that i had forgot the actual handler for submit aws events/submit commands

--- handler.js
+++ handler.js
@@ -45,3 +45,28 @@ module.exports.updateTimesheet = (event, context, callback) => {
             callback(new Error('[404] Entity Not found - ' + JSON.stringify(event) + ' - error: ' + JSON.stringify(error)));
           });  
 };
+module.exports.submitTimesheet = (event, context, callback) => {
+  let timesheetId = event.path.id
+  repository.hydrateAggregate(new Timesheet(timesheetId))
+          .then((timesheet)=> {
+            timesheet.submit(event);
+            repository.saveAggregate(timesheet)
+              .then((unprocessedEntities)=> {
+                if(unprocessedEntities.length !=0)
+                  console.error({"unprocessedEntities" :unprocessedEntities}); //Um what to do here?
+                console.log("Timesheet submitted. JSON:", JSON.stringify(timesheet));
+                let response = { message: 'Timesheet Sumitted', timesheetId: timesheet.id , data: timesheet};
+                callback(null, response)
+              })
+              .catch((error) => {
+                console.error(error);
+                console.error("Unable to save timesheet . Error JSON:", JSON.stringify(error));
+                callback(new Error('[422] Unprocessable Entity - ' + JSON.stringify(event) + ' - error: ' + JSON.stringify(error)));
+              })
+          })
+          .catch((error) => {
+            console.error(error);
+            console.error("Unable to retrieve timesheet . Error JSON:", JSON.stringify(error));
+            callback(new Error('[404] Entity Not found - ' + JSON.stringify(event) + ' - error: ' + JSON.stringify(error)));
+          });  
+};
